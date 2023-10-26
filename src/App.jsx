5ae49b00035516071bf5a729aad97e03ca812184
khanshorif331 <khanshorif331@gmail.com>
2023-10-26 13:38:45 +0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import emaLogo from './images/Logo.svg'
+import './App.css'
+import Header from './components/header/header'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
