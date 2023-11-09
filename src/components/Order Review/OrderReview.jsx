8337e899f0e106665a4101fa66e3a230ad077d8b
khanshorif331 <khanshorif331@gmail.com>
2023-11-09 13:25:51 +0600

    implemented delete from cart

--- src/components/Order Review/OrderReview.jsx
+++ src/components/Order Review/OrderReview.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './review.css'
-const OrderReview = ({product}) => {
-    const {img, price, name, shipping } = product
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+const OrderReview = ({product, handleDeleteFromCart}) => {
+    const {id, img, price, name, shipping } = product
     return (
         <div className='review-details'>
             <img src={img} alt="" />
@@ -10,10 +12,10 @@ const OrderReview = ({product}) => {
                 <p>{name}</p>
                 <p>Price: <span>${price} </span></p>
                 <p>Shipping Charge: <span>${shipping} </span> </p>
-            </div>
-            <p className='delete'>d</p>
         </div>
-            {/* <button className='delete-button'>d</button> */}
+            {/* <p className='delete'>d</p> */}
+            <button onClick={()=> handleDeleteFromCart(id)} className='delete-button'><FontAwesomeIcon icon= {faTrashAlt} /></button>
+        </div>
         </div>
     );
 };
--- src/components/orders/Orders.jsx
+++ src/components/orders/Orders.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../cart/cart';
 import OrderReview from '../Order Review/OrderReview';
-
-
+import './orders.css'
 const Orders = () => {
-    const cart = useLoaderData()
+    const savedCart = useLoaderData()
+    const [cart, setCart] =useState(savedCart)
+    const handleDeleteFromCart=(id)=>{
+        const newProduct = cart.filter(product=> product.id !== id)
+        setCart(newProduct)
+        removeFromDb(id)
+    }
     return (
         <div className='container'>
-            <div>
+            <div className='review-container'>
             {
-                cart.map(product => <OrderReview key={product.id} product ={product} ></OrderReview>)
+                cart.map(product => <OrderReview key={product.id} product ={product} handleDeleteFromCart={handleDeleteFromCart}></OrderReview>)
             }
             </div>
             <div className='cart'>
